fa6f0463af7989e0827d3b60dd29af5aa8514c40
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-09-19 14:02:24 +0900

    2023-09-19

---
+++ src/api/video.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// http://loacalhost:8080/api/
+const instance = axios.create({
+    baseURL: "http://localhost:8080/api/"
+});
+
+   // async ~ await + axios
+export const getCategories = async () => { // 요게 프론트에서 함수임
+    return await instance.get("category") // 요건 변수
+
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import Router from "./router";
+import "./assets/reset.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={Router} />);
+
